3fac510ac4e029e84e6d927e04d427f6072e2add
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-10 15:50:18 +0200

    Average speed check routes

--- routes/avgSpeedChecks.js
+++ routes/avgSpeedChecks.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
+let AvgSpeedCheck = mongoose.model("AvgSpeedCheck");
+let jwt = require('express-jwt');
+
+let auth = jwt({ secret: process.env.FLITS_BACKEND_SECRET });
 
 /* GET avgSpeedChecks listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    let query = AvgSpeedCheck.find().populate("User");
+    query.exec(function(err, avgSpeedChecks) {
+        if (err) return next(err);
+        res.json(avgSpeedChecks);
+    });
+});
+
+/* GET avgSpeedCheck by id. */
+router.param("avgSpeedCheckId", function (req, res, next, id) {
+    let query = AvgSpeedCheck.findById(id).populate("User");
+    query.exec(function (err, avgSpeedCheck) {
+        if (err) return next(err);
+        if (!avgSpeedCheck) return next(new Error("not found " + id));
+        req.avgSpeedCheck = avgSpeedCheck;
+        return next();
+    });
+});
+
+router.get("/:avgSpeedCheckId", function (req, res, next) {
+    res.json(req.avgSpeedCheck);
+});
+
+/* POST avgSpeedCheck */
+router.post("/", auth, function (req, res, next) {
+    let avgSpeedCheck = new AvgSpeedCheck({
+        beginLocation: req.body.beginLocation,
+        endLocation: req.body.endLocation,
+        description: req.body.description,
+        timeCreated: req.body.timeCreated,
+        user: req.time.user
+    });
+    avgSpeedCheck.save(function (err, avgSpeedCheck) {
+        if (err) return next(err);
+        res.json(avgSpeedCheck);
+    });
+});
+
+/* DELETE avgSpeedCheck */
+router.delete("/:avgSpeedCheckId", auth, function (req, res, next) {
+    req.avgSpeedCheck.remove(function (err) {
+        if (err) return next(err);
+        res.json(req.avgSpeedCheck);
+    });
 });
 
 module.exports = router;
