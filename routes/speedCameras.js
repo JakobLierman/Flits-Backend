7dd054fe578b58ef216c97c289e7bff7b9e732e2
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-10 15:42:49 +0200

    Speed camera routes

--- routes/speedCameras.js
+++ routes/speedCameras.js
@@ -15,4 +15,44 @@ router.get('/', function(req, res, next) {
     });
 });
 
+/* GET speedCamera by id. */
+router.param("speedCameraId", function (req, res, next, id) {
+    let query = SpeedCamera.findById(id).populate("User");
+    query.exec(function (err, speedCamera) {
+        if (err) return next(err);
+        if (!speedCamera) return next(new Error("not found " + id));
+        req.speedCamera = speedCamera;
+        return next();
+    });
+});
+
+router.get("/:speedCameraId", function (req, res, next) {
+    res.json(req.speedCamera);
+});
+
+/* POST speedCamera */
+router.post("/", auth, function (req, res, next) {
+    let speedCamera = new SpeedCamera({
+        location: req.body.location,
+        kind: req.body.kind,
+        description: req.body.description,
+        imagePath: req.body.imagePath,
+        timeCreated: req.body.timeCreated,
+        expireDate: req.time.expireDate,
+        user: req.time.user
+    });
+    speedCamera.save(function (err, speedCamera) {
+        if (err) return next(err);
+        res.json(speedCamera);
+    });
+});
+
+/* DELETE speedCamera */
+router.delete("/:speedCameraId", auth, function (req, res, next) {
+    req.speedCamera.remove(function (err) {
+        if (err) return next(err);
+        res.json(req.speedCamera);
+    });
+});
+
 module.exports = router;
